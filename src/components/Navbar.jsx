7f73e95385236519bd389826374e0b4f035f1dfc
front-ed <edwinschutjes@gmail.com>
2023-07-29 18:14:55 +0200

    component: sidebar + functions + styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,20 @@
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "../context";
+
 const Navbar = () => {
-  return <p>Navbar</p>;
+  const { openSidebar } = useGlobalContext();
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar} type="button">
+          <FaBars />
+        </button>
+        {/* ToDo: add NAV links */}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
